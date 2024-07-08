8a887b496bba5fefb57984a125c2cbc975d11ebe
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-07-08 13:56:14 -0500

    feat: :sparkles: Ejercicio 2
    Crea una clase llamada Animal con las siguientes propiedades: nombre y edad. La clase debe tener un método llamado hacer Sonido que imprima en la consola el sonido del animal. Crea una instancia de la clase Animal llamada animal1 y asigna valores a sus propiedades. Luego, llama al método hacerSonido() para que el animal imprima su sonido. Crea una clase llamada Perro que herede de la clase Animal. Agrega una nueva propiedad llamada raza y un método llamado moverCola que imprima en la consola un mensaje indicando que el perro está moviendo la cola

--- js/main.js
+++ js/main.js
@@ -1,72 +1,60 @@
-class Persona {
-    constructor(nombre, edad, sexo) {
+class Animal {
+    constructor(nombre, edad) {
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
     }
 
-    static esMayorDeEdad(edad) {
-        return edad >= 18;
-    }
-
-    saludar() {
-        console.log(`Hey, soy ${this.nombre}, tengo ${this.edad} años y soy ${this.sexo}`);
+    hacerSonido() {
+        console.log(`${this.nombre} está haciendo un sonido`);
+        
+        return `${this.nombre} está haciendo un sonido`;
     }
 }
 
-class Estudiante extends Persona {
-    constructor(nombre, edad, sexo, carrera) {
-        super(nombre, edad, sexo);
-        this.carrera = carrera;
+class Perro extends Animal {
+    constructor(nombre, edad, raza) {
+        super(nombre, edad);
+        this.raza = raza;
     }
 
-    estudiar() {
-        console.log(`Estoy estudiando ${this.carrera}`);
+    moverCola() {
+        console.log(`está moviendo la cola.`);
+
+        return `está moviendo la cola.`;
     }
 }
 
 document.querySelector(".btn").addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     const nombre = document.querySelector("#name").value;
     const edad = parseInt(document.querySelector("#age").value);
-    const sexo = document.querySelector("#sex").value;
-    const carrera = document.querySelector("#career").value;
+    const raza = document.querySelector("#breed").value;
 
-    if (carrera) { // Si se ingresó una carrera en el form 
-        const estudiante = new Estudiante(nombre, edad, sexo, carrera);
-        estudiante.saludar();
-        estudiante.estudiar();
-    } else {
-        const persona = new Persona(nombre, edad, sexo);
-        persona.saludar();
-    }
+    const animal1 = new Animal(nombre, edad);
+    const sonidoAnimal = animal1.hacerSonido();
 
-    const esMayor = Persona.esMayorDeEdad(edad);
-    console.log(`¿${nombre} es mayor de edad? ${esMayor ? 'si' : 'no'}`);
+    const perro1 = new Perro(nombre, edad, raza);
+    const sonidoPerro = perro1.hacerSonido();
+    const accionPerro = perro1.moverCola();
 
-    const header = document.createElement("h2");
-    header.textContent = "Submitted !";
-    document.querySelector(".header").appendChild(header);
-    
-    setTimeout(() => {
-        header.remove();
-    }, 3000);   
+    const result = document.createElement("p");
 
-    document.querySelector("#signup-form").reset();
-
-    const result = document.createElement("p")
-
-    if (carrera) {
-        result.textContent = `Hey, soy ${nombre}, tengo ${edad} años, soy ${sexo}, estudio ${carrera} y ${esMayor ? 'si' : 'no'} soy mayor de edad`;
-    } else {
-        result.textContent = `Hey, soy ${nombre}, tengo ${edad} años, soy ${sexo} y ${esMayor} soy mayor de edad`;
-    }
+    result.textContent = raza ? `${sonidoPerro} y ${accionPerro}` : sonidoAnimal;
 
     document.querySelector(".result").appendChild(result);
 
     setTimeout(() => {
         result.remove();
-    }, 5000); 
+    }, 5000);
 
+    document.querySelector("#signup-form").reset();
+
+    const header = document.createElement("h2");
+    header.textContent = "Submitted!";
+    document.querySelector(".header").appendChild(header);
+
+    setTimeout(() => {
+        header.remove();
+    }, 3000);
 });
